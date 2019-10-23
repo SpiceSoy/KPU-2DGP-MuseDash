6bc53d824bc9110bd7f73f6173b807b14c8cc335
SpiceSoy <aksghkgh12@gmail.com>
2019-10-24 01:11:33 +0900

    리소스 저장 모듈로 이동

--- RhythmCrush/utill/image_manager.py
+++ RhythmCrush/utill/image_manager.py
@@ -1,32 +1,36 @@
+import copy
 import pico2d
 from RhythmCrush import debug
-
-image_base_dir_url = "Resource/Image/"
-
-image_url_dic = {
-        'note-back-big': "Note/taikobigcircle.png",
-        'note-front-big': "Note/taikobigcircleoverlay.png",
-        'note-back': "Note/taikohitcircle.png",
-        'note-front': "Note/taikohitcircleoverlay.png",
-        'player-trex': "Char/t_rex_sprite.png"
-    }
-
-image_cache = {}
+from ..component.image_controller import *
+from . import ResourceData
 
 debug.print_console('image_manager', "Init")
 
 
 # 동기적 작업
 def load_image(tag):
-    if tag in image_url_dic:
-        if tag in image_cache:
+    if tag in ResourceData.image_url_dic:
+        if ResourceData.image_url_dic[tag] in ResourceData.image_cache:
             debug.print_console('image_manager', f"Load Image {tag} at Cache .")
-            ret_image = image_cache[tag]
+            ret_image = ResourceData.image_cache[ResourceData.image_url_dic[tag]]
         else:
-            complete_url = image_base_dir_url + image_url_dic[tag]
+            complete_url = ResourceData.image_base_dir_url + ResourceData.image_url_dic[tag]
             debug.print_console('image_manager', f"Load Image {tag} at {complete_url} .")
             ret_image = pico2d.load_image(complete_url)
-            image_cache[tag] = ret_image
+            ResourceData.image_cache[ResourceData.image_url_dic[tag]] = ret_image
         return ret_image
     else:
         debug.print_console('image_manager', f"Image Dic has NOT Image {tag}")
+
+
+def load_animator(tag):
+    if tag in ResourceData.animators:
+        return copy.copy(ResourceData.animators[tag])
+    else:
+        return None
+
+
+def get_image_controller(tag):
+    result = ImageController(load_image(tag))
+    result.add_animator(load_animator(tag))
+    return result
