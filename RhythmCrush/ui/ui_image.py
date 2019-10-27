126f1a946fed11739c88b1452e1048403e4fadaa
SpiceSoy <aksghkgh12@gmail.com>
2019-10-28 02:24:26 +0900

    UI 정적 이미지 추가 (테스트 안해봄)

---
+++ RhythmCrush/ui/ui_image.py
@@ -0,0 +1,22 @@
+import enum
+
+from ..ui.ui_base import *
+
+from ..utill import image_manager
+
+
+class UIStaticImage(BaseUIObject):
+    def __init__(self, x, y, tag, w=None, h=None):
+        super().__init__(x, y)
+        self.image_tag = tag
+        self.image_controller = None
+        self.w = w
+        self.h = h
+
+    def load(self):
+        self.image_controller = image_manager.get_image_controller(self.image_tag)
+
+    def draw(self):
+        self.image_controller.draw(self.position[0], self.position[1], self.w, self.h)
+
+
