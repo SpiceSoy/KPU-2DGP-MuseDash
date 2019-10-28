cfd03ad197fcebfb54727a67ed63baada0f9fe12
SpiceSoy <aksghkgh12@gmail.com>
2019-10-29 01:21:39 +0900

    포지션값 리드온리 해제 (튜플 -> 리스트)

--- RhythmCrush/ui/ui_base.py
+++ RhythmCrush/ui/ui_base.py
@@ -3,7 +3,7 @@ from ..interface import IUpdatableObject, IDrawableObject
 
 class BaseUIObject(IUpdatableObject, IDrawableObject):
     def __init__(self, x, y):
-        self.position = (x, y)
+        self.position = [x, y]
 
     def update(self, delta_time):
         pass
