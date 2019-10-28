9ab76ce37ca0f735a403e53f1b007c11ce074cf5
SpiceSoy <aksghkgh12@gmail.com>
2019-10-29 01:21:55 +0900

    타이틀 씬 버튼 및 키입력 감지 추가

--- RhythmCrush/game_scene/title_scene.py
+++ RhythmCrush/game_scene/title_scene.py
@@ -3,29 +3,95 @@ from ..game_scene.base_scene import BaseScene
 from .. import handler_set
 from ..ui import *
 
+from ..game_scene import *
+
 import pico2d
 
 
 class TitleScene(BaseScene):
     def __init__(self, framework):
         super().__init__(framework)
-        self.title_image = None
+        self.csr = 0
+        self.csr_y = [
+            framework.h - 561,
+            framework.h - 690
+        ]
+        self.button = [
+            ClickableRect(1162, framework.h - 561, 350, 120),
+            ClickableRect(1162, framework.h - 690, 350, 120)
+        ]
+        self.title_image = UIStaticImage(self.framework.w/2, self.framework.h/2, 'ui-title-back')
+        self.csr_image = UIStaticImage(980, self.csr_y[self.csr], 'ui-csr')
 
     def load(self):
-        self.title_image = UIStaticImage(self.framework.w/2, self.framework.h/2, 'ui-title-back')
+        super().load()
+        self.title_image.load()
+        self.csr_image.load()
 
     def update(self, delta_time):
+        self.csr_image.position[1] = self.csr_y[self.csr]
         pass
 
     def draw(self):
         self.title_image.draw()
+        self.csr_image.draw()
 
     def post_handler(self):
         def game_end():
             self.framework.exit()
 
+        def arrow_up():
+            self.csr = pico2d.clamp(0, self.csr-1, 1)
+
+        def arrow_down():
+            self.csr = pico2d.clamp(0, self.csr+1, 1)
+
+        def setcsr(csr):
+            def ret():
+                self.csr = csr
+            return  ret
+
+        def move_next_scene():
+            self.framework.change_scene(
+                NotePlayScene(self, "Resource/Map/Second/Camellia - Chirality (_DUSK_) [Muzukashii].osu")
+            )
+
+        def menu_func():
+            if self.csr == 1:
+                self.framework.exit()
+            elif self.csr == 0:
+                move_next_scene()
+
         self.input_handler.add_handler(
             pico2d.SDL_KEYDOWN,
-            handler_set.key_input(pico2d.SDLK_DOWN, game_end)
+            handler_set.key_input(pico2d.SDLK_ESCAPE, game_end)
+        )
+        self.input_handler.add_handler(
+            pico2d.SDL_KEYDOWN,
+            handler_set.key_input(pico2d.SDLK_UP, arrow_up)
+        )
+        self.input_handler.add_handler(
+            pico2d.SDL_KEYDOWN,
+            handler_set.key_input(pico2d.SDLK_DOWN, arrow_down)
+        )
+        self.input_handler.add_handler(
+            pico2d.SDL_MOUSEMOTION,
+            handler_set.mouse_motion_input(setcsr(0), self.button[0])
+        )
+        self.input_handler.add_handler(
+            pico2d.SDL_MOUSEMOTION,
+            handler_set.mouse_motion_input(setcsr(1), self.button[1])
+        )
+        self.input_handler.add_handler(
+            pico2d.SDL_KEYDOWN,
+            handler_set.key_input(pico2d.SDLK_RETURN, menu_func)
+        )
+        self.input_handler.add_handler(
+            pico2d.SDL_MOUSEBUTTONDOWN,
+            handler_set.mouse_button_input(pico2d.SDL_BUTTON_LEFT, menu_func, self.button[1])
+        )
+        self.input_handler.add_handler(
+            pico2d.SDL_MOUSEBUTTONDOWN,
+            handler_set.mouse_button_input(pico2d.SDL_BUTTON_LEFT, move_next_scene, self.button[0])
         )
         pass
