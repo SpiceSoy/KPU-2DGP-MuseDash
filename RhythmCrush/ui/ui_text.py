144afa18faa77edce7e86655c5a5f503fd3a3283
SpiceSoy <aksghkgh12@gmail.com>
2019-11-09 02:33:49 +0900

    UI 텍스트 컬러 변경 함수 추가

--- RhythmCrush/ui/ui_text.py
+++ RhythmCrush/ui/ui_text.py
@@ -6,7 +6,8 @@ from ..utill.font_manager import *
 
 
 class UIText(BaseUIObject):
-    def __init__(self, x, y, text: str = "", font=FontType.NanumSquare, style=FontStyle.Regular, pt=20, color=(0, 0, 0)):
+    def __init__(self, x, y, text: str = "", font=FontType.NanumSquare, style=FontStyle.Regular, pt=20,
+                 color=(0, 0, 0)):
         super().__init__(x, y)
         self.text = text
         self.font_type = font
@@ -20,6 +21,9 @@ class UIText(BaseUIObject):
 
     def update_text(self, text):
         self.text = text
+
+    def change_color(self, color=(0, 0, 0)):
+        self.color = color
 
     def draw(self):
         self.font.draw(self.position[0], self.position[1], self.text, self.color)
