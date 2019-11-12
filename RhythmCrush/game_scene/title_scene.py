1345fcb0123ccdcbc0f91e566b1393fa32f132a8
SpiceSoy <aksghkgh12@gmail.com>
2019-11-13 03:07:44 +0900

    타이틀 씬 게임월드 적용 및 리팩토링 1차

--- RhythmCrush/game_scene/title_scene.py
+++ RhythmCrush/game_scene/title_scene.py
@@ -1,9 +1,8 @@
 from ..game_scene.base_scene import BaseScene
-from ..game_scene import select_scene
+from ..game_scene.select_scene import SelectScene
 
 from .. import handler_set
 from ..ui import *
-
 
 import pico2d
 
@@ -23,20 +22,15 @@ class TitleScene(BaseScene):
         self.title_image = UIStaticImage(self.framework.w/2, self.framework.h/2, 'ui-title-back')
         self.csr_image = UIStaticImage(980, self.csr_y[self.csr], 'ui-csr')
 
-    def load(self):
-        super().load()
-        self.title_image.load()
-        self.csr_image.load()
+        self.game_world.add_object(self.title_image, 0)
+        self.game_world.add_object(self.csr_image, 1)
 
     def update(self, delta_time):
         self.csr_image.position[1] = self.csr_y[self.csr]
-        pass
-
-    def draw(self):
-        self.title_image.draw()
-        self.csr_image.draw()
+        super().update(delta_time)
 
     def post_handler(self):
+        # 콜백 함수 선언 시작
         def game_end():
             self.framework.exit()
 
@@ -46,24 +40,21 @@ class TitleScene(BaseScene):
         def arrow_down():
             self.csr = pico2d.clamp(0, self.csr+1, 1)
 
-        def set_csr(csr):
+        # 고차 함수
+        def get_csr_set_func(csr):
             def ret():
                 self.csr = csr
             return ret
 
         def move_next_scene():
-            # self.framework.change_scene(
-            #     game_map.NotePlayScene(self.framework, "Resource/Map/Third/BlackY - Max Burning!! (SpectorDG) [Senritsu's Futsuu].osu")
-            # )
-            self.framework.change_scene(
-                select_scene.SelectScene(self.framework)
-            )
+            self.framework.change_scene(SelectScene(self.framework))
 
-        def menu_func():
+        def menu_enter_func():
             if self.csr == 1:
                 self.framework.exit()
             elif self.csr == 0:
                 move_next_scene()
+        # 콜백 함수 선언 종료
 
         self.input_handler.add_handler(
             pico2d.SDL_KEYDOWN,
@@ -79,22 +70,21 @@ class TitleScene(BaseScene):
         )
         self.input_handler.add_handler(
             pico2d.SDL_MOUSEMOTION,
-            handler_set.mouse_motion_input(set_csr(0), self.button[0])
+            handler_set.mouse_motion_input(get_csr_set_func(0), self.button[0])
         )
         self.input_handler.add_handler(
             pico2d.SDL_MOUSEMOTION,
-            handler_set.mouse_motion_input(set_csr(1), self.button[1])
+            handler_set.mouse_motion_input(get_csr_set_func(1), self.button[1])
         )
         self.input_handler.add_handler(
             pico2d.SDL_KEYDOWN,
-            handler_set.key_input(pico2d.SDLK_RETURN, menu_func)
+            handler_set.key_input(pico2d.SDLK_RETURN, menu_enter_func)
         )
         self.input_handler.add_handler(
             pico2d.SDL_MOUSEBUTTONDOWN,
-            handler_set.mouse_button_input(pico2d.SDL_BUTTON_LEFT, menu_func, self.button[1])
+            handler_set.mouse_button_input(pico2d.SDL_BUTTON_LEFT, menu_enter_func, self.button[1])
         )
         self.input_handler.add_handler(
             pico2d.SDL_MOUSEBUTTONDOWN,
             handler_set.mouse_button_input(pico2d.SDL_BUTTON_LEFT, move_next_scene, self.button[0])
         )
-        pass
