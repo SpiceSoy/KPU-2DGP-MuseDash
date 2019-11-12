09dfe61c252c440a9245236bd19241381238ccfd
SpiceSoy <aksghkgh12@gmail.com>
2019-11-13 03:00:42 +0900

    게임 월드 버그 수정

--- RhythmCrush/game_object/game_world.py
+++ RhythmCrush/game_object/game_world.py
@@ -4,13 +4,13 @@ class GameWorld(IUpdatableObject, IDrawableObject):
     def __init__(self,layer_count: int = 3):
         self.obj_layer = []
         for i in range(3):
-            self.obj_layer += []
+            self.obj_layer.append([])
 
     def __del__(self):
         self.clear()
 
     def add_object(self, obj, layer):
-        self.obj_layer[layer] += obj
+        self.obj_layer[layer].append(obj)
 
     def delete_object(self, obj):
         for i in range(len(self.obj_layer)):
@@ -19,7 +19,7 @@ class GameWorld(IUpdatableObject, IDrawableObject):
                 del obj
 
     def clear(self):
-        for obj in self.all_object()
+        for obj in self.all_object():
             del obj
 
     def all_object(self):
@@ -27,10 +27,14 @@ class GameWorld(IUpdatableObject, IDrawableObject):
             for o in self.obj_layer[i]:
                 yield o
 
+    def load(self):
+        for o in self.all_object():
+            o.load()
+
     def update(self, delta_time):
-        for o in self.all_obj():
+        for o in self.all_object():
             o.update(delta_time)
 
     def draw(self):
-        for o in self.all_obj():
+        for o in self.all_object():
             o.draw()
