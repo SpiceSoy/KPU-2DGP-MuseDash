e51cde6fc13005e2c6d126de34d080a5d913c45c
SpiceSoy <aksghkgh12@gmail.com>
2019-11-15 04:14:13 +0900

    이미지 컨트롤러 알파값 조절 기능 추가

--- RhythmCrush/component/image_controller.py
+++ RhythmCrush/component/image_controller.py
@@ -8,6 +8,10 @@ class ImageController(IUpdatableObject):
         self.animator = None
         self.speed = 1
         self.image = image
+        self.alpha = 1.0
+
+    def set_alpha(self, alpha):
+        self.alpha = alpha
 
     def add_animator(self, animator: Animator):
         self.animator = animator
@@ -22,6 +26,7 @@ class ImageController(IUpdatableObject):
         return self.speed
 
     def draw(self, x, y, w=None, h=None):
+        self.image.opacify(self.alpha)
         if self.animator is None:
             self.image.draw(x, y, w, h)
         else:
@@ -29,9 +34,11 @@ class ImageController(IUpdatableObject):
             self.image.clip_draw(frame[0], frame[1], frame[2], frame[3], x, y, w, h)
 
     def clip_draw_no_animation(self, left, bottom, width, height, x, y, w=None, h=None):
+        self.image.opacify(self.alpha)
         self.image.clip_draw(left, bottom, width, height, x, y, w, h)
 
     def composite_draw(self, rad, flip, x, y, w=None, h=None):
+        self.image.opacify(self.alpha)
         if self.animator is None:
             self.image.composite_draw(rad, flip, x, y, w, h)
         else:
@@ -39,6 +46,7 @@ class ImageController(IUpdatableObject):
             self.image.clip_composite_draw(rad, flip, frame[0], frame[1], frame[2], frame[3], x, y, w, h)
 
     def clip_composite_draw_no_animation(self, rad, flip, left, bottom, width, height, x, y, w=None, h=None):
+        self.image.opacify(self.alpha)
         self.image.clip_composite_draw(left, bottom, width, height, x, y, rad, flip, w, h)
 
     def update(self, delta_time):
