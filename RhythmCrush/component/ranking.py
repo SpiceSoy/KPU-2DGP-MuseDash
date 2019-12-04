60c151f9ffe35288748e129a79cd9f7c296a1560
SpiceSoy <aksghkgh12@gmail.com>
2019-12-05 03:55:56 +0900

    랭킹 함수 이름 수정 및 데이터 추출 추가

--- RhythmCrush/component/ranking.py
+++ RhythmCrush/component/ranking.py
@@ -23,10 +23,22 @@ class Ranking:
     def __init__(self):
         self.data = {}
 
-    def PostRank(self, rank: RankData):
+    def post_rank(self, rank: RankData):
         if rank.music_name not in self.data:
             self.data[rank.music_name] = {0: [], 1: [], 2: []}
         self.data[rank.music_name][rank.difficult].append(rank.to_save_dict())
+
+    def get_rank_list(self, music_name, difficult):
+        data = []
+        for dic in self.data[music_name][difficult]:
+            ele = RankData()
+            ele.music_name = music_name
+            ele.difficult = difficult
+            ele.score = int(dic["score"])
+            ele.max_combo = int(dic["max_combo"])
+            ele.accuracy = int(dic["accuracy"])
+            data.append(ele)
+        return data
 
     def dump(self):
         with open(Ranking.rank_file_dir, 'w', encoding='utf-8') as make_file:
