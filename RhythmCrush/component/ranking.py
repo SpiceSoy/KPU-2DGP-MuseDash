dc7386269931364dce97fdec20ecaa4e732ee43d
SpiceSoy <aksghkgh12@gmail.com>
2019-12-07 01:33:26 +0900

    랭킹 모듈 제작

--- RhythmCrush/component/ranking.py
+++ RhythmCrush/component/ranking.py
@@ -1,5 +1,5 @@
 import json
-
+import os
 
 class RankData:
     def __init__(self):
@@ -17,33 +17,40 @@ class RankData:
         }
 
 
-class Ranking:
-    rank_file_dir = "Data/rank.json"
+rank_file_dir = "Data/rank.json"
+rank_data = {}
 
-    def __init__(self):
-        self.data = {}
 
-    def post_rank(self, rank: RankData):
-        if rank.music_name not in self.data:
-            self.data[rank.music_name] = {0: [], 1: [], 2: []}
-        self.data[rank.music_name][rank.difficult].append(rank.to_save_dict())
+def post_rank(rank: RankData):
+    global rank_data
+    if rank.music_name not in rank_data:
+        rank_data[rank.music_name] = {0: [], 1: [], 2: []}
+    rank_data[rank.music_name][str(rank.difficult)].append(rank.to_save_dict())
+    dump()
 
-    def get_rank_list(self, music_name, difficult):
-        data = []
-        for dic in self.data[music_name][difficult]:
-            ele = RankData()
-            ele.music_name = music_name
-            ele.difficult = difficult
-            ele.score = int(dic["score"])
-            ele.max_combo = int(dic["max_combo"])
-            ele.accuracy = int(dic["accuracy"])
-            data.append(ele)
-        return data
+def get_rank_list(music_name, difficult):
+    global rank_data
+    data = []
+    for dic in rank_data[music_name][difficult]:
+        ele = RankData()
+        ele.music_name = music_name
+        ele.difficult = difficult
+        ele.score = int(dic["score"])
+        ele.max_combo = int(dic["max_combo"])
+        ele.accuracy = int(dic["accuracy"])
+        data.append(ele)
+    return data
 
-    def dump(self):
-        with open(Ranking.rank_file_dir, 'w', encoding='utf-8') as make_file:
-            json.dump(self.data, make_file, indent="\t")
+def dump():
+    global rank_data
+    with open(rank_file_dir, 'w', encoding='utf-8') as make_file:
+        print("DUMPPED")
+        json.dump(rank_data, make_file, indent="\t")
 
-    def load(self):
-        with open(Ranking.rank_file_dir, 'w', encoding='utf-8') as load_file:
-            self.data = json.load(load_file)
+def load():
+    global rank_data
+    if os.path.isfile(rank_file_dir):
+        with open(rank_file_dir, 'r', encoding='utf-8') as load_file:
+            rank_data = json.load(load_file)
+
+load()
